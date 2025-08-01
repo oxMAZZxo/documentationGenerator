55e02dff7c8b8af3a8fb811e7811f3a83c5d3ada
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2025-08-01 21:02:58 +0100

    Created Models for Classes and Enums and base Declaration

---
+++ DocumentationGenerator/DocumentationGenerator/MVVM/Model/ClassDeclaration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationGenerator.MVVM.Model
+{
+    /// <summary>
+    /// A Class Declaration is a class that is declared within a script file (.cs for example), and can store the classes name, and definition (if has one), along with its fields, properties and methods (and functions).
+    /// </summary>
+    public struct ClassDeclaration
+    {
+        public string Name { get; }
+        public string? Definition { get; }
+        public Declaration[]? Methods { get; }
+        public Declaration[]? Fields { get; }
+        public Declaration[]? Properties { get; }
+
+        public ClassDeclaration(string name, string? Definition, Declaration[]? methodDeclarations, Declaration[]? fieldDeclarations, Declaration[]? propertiesDeclarations)
+        {
+            this.Name = name;
+            this.Definition = Definition;
+            this.Methods = methodDeclarations;
+            this.Fields = fieldDeclarations;
+            this.Properties = propertiesDeclarations;
+        }
+    }
+
+    /// <summary>
+    /// A declaration refers to anything declared within a script file. This could be classes, enums, interfaces, fields, properties, methods.
+    /// This can be used to stored any of these declarations along with their definition, types and return definitions.
+    /// </summary>
+    public struct Declaration
+    {
+        public string Name { get; set; }
+        public string? Type { get; set; }
+        public string? Definition { get; set; }
+        public string? ReturnDefinition { get; set; }
+
+        public Declaration(string name, string? definition, string? type = null, string? returns = null)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Definition = definition;
+            this.ReturnDefinition = returns;
+        }
+    }
+}
---
+++ DocumentationGenerator/DocumentationGenerator/MVVM/Model/EnumDeclaration.cs
@@ -0,0 +1,16 @@
+namespace DocumentationGenerator.MVVM.Model
+{
+    public struct EnumDeclaration
+    {
+        public string Name { get; }
+        public string? Definition { get; }
+        public Declaration[] EnumMembers { get; }
+
+        public EnumDeclaration(string name, string? definition, Declaration[] members)
+        {
+            Name = name;
+            Definition = definition;
+            EnumMembers = members;
+        }
+    }
+}
