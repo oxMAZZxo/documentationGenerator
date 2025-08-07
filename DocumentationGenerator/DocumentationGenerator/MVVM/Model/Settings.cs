235c2681be0d9e4a8037f06fb3380b62730bf06b
Mazz <mariosgamakapa@gmail.com>
2025-08-07 14:06:36 +0100

    Create Settings.cs

---
+++ DocumentationGenerator/DocumentationGenerator/MVVM/Model/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationGenerator.MVVM.Model
+{
+    public class Settings : IDisposable
+    {
+        public static Settings? Instance { get; private set; }
+
+        public Settings()
+        {
+            if(Instance != null)
+            {
+                return;
+            }
+
+            if(Instance == null && Instance != this)
+            {
+                Instance = this;
+            }
+        }
+
+        ~Settings()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
