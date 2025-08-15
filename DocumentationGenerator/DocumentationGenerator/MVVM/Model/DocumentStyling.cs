d6fea234187451e61938a0b7124c3f0d05f8d7f9
Mazz <mariosgamakapa@gmail.com>
2025-08-15 11:14:39 +0100

    Create DocumentStyling.cs

---
+++ DocumentationGenerator/DocumentationGenerator/MVVM/Model/DocumentStyling.cs
@@ -0,0 +1,36 @@
+﻿using MigraDoc.DocumentObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationGenerator.MVVM.Model
+{
+    public class DocumentStyling
+    {
+        //public DeclarationColours DeclarationColours { get; set; }
+        public DeclarationFonts DeclarationFonts { get; set; }
+        public bool GenerateTableOfContents { get; set; }
+        public bool GeneratePageNumbers { get; set; }
+
+    }
+
+    public struct DeclarationFonts
+    {
+        public string FontFamilyName { get; set; }
+        public FontDeclarationStyle PrimitiveDeclarationFontStyle { get; set; }
+        public FontDeclarationStyle ClassDeclarationFontStyle { get; set; }
+        public FontDeclarationStyle StructDeclarationFontStyle { get; set; }
+        public FontDeclarationStyle EnumDeclarationFontStyle { get; set; }
+        public FontDeclarationStyle InterfaceDeclarationFontStyle { get; set; }
+    }
+
+    public struct FontDeclarationStyle
+    {
+        public int FontSize { get; set; }
+        public Color Color { get; set; }
+        public bool IsItalic { get; set; }
+        public bool IsBold { get; set; }
+    }
+}
