94bb9d697f063d5eb5fb87dc1572f1e85028630f
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2025-09-26 15:39:17 +0100

    Started Implementing the new Template in the HTML Writer

--- DocumentationGenerator/DocumentationGenerator/MVVM/Model/DocumentationWriters/HtmlWriter.cs
+++ DocumentationGenerator/DocumentationGenerator/MVVM/Model/DocumentationWriters/HtmlWriter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,23 +13,31 @@ namespace DocumentationGenerator.MVVM.Model.DocumentationWriters
         public bool Write(string path, ClassDeclaration[]? classes, EnumDeclaration[]? enums, InterfaceDeclaration[]? interfaces, StructDeclaration[]? structs, DocumentInformation docInfo)
         {
             if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path)) { return false; }
+
             DirectoryInfo outputPath = Directory.CreateDirectory(Path.Combine(path, $"{docInfo.ProjectName} Documentation"));
 
+            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/helper.js"), Path.Combine(outputPath.FullName, "helper.js"), true);
+            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/helper.js"), Path.Combine(outputPath.FullName, "docHelpers.js"), true);
+            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/homePageStyles.css"), Path.Combine(outputPath.FullName, "homePageStyles.css"), true);
+            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/homePageStyles.css"), Path.Combine(outputPath.FullName, "sidebar.css"), true);
+            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/homePageStyles.css"), Path.Combine(outputPath.FullName, "docStyles.css"), true);
             GenerateHomePage(outputPath.FullName, classes, enums, interfaces, structs, docInfo);
             GenerateObjPages(outputPath.FullName, classes, enums, interfaces, structs, docInfo);
+
             if (docInfo.GenerateRelationshipGraph)
             {
                 // string graphPath = GenerateRelationshipGraph(classes, interfaces, documentStyling.DeclarationColours, path);
             }
-
-            Directory.CreateDirectory(Path.Combine(outputPath.FullName, "objs"));
-
             return true;
         }
 
         private void GenerateObjPages(string fullName, ClassDeclaration[]? classes, EnumDeclaration[]? enums, InterfaceDeclaration[]? interfaces, StructDeclaration[]? structs, DocumentInformation docInfo)
         {
-
+            DirectoryInfo objsDirectory = Directory.CreateDirectory(Path.Combine(fullName, "objs"));
+            if (classes != null && classes.Length > 0)
+            {
+                //GenerateClasses(objsDirectory.FullName, classes, docInfo);
+            }
         }
 
         private string GenerateHomePage(string outputPath, ClassDeclaration[]? classes, EnumDeclaration[]? enums, InterfaceDeclaration[]? interfaces, StructDeclaration[]? structs, DocumentInformation docInfo)
@@ -36,141 +45,169 @@ namespace DocumentationGenerator.MVVM.Model.DocumentationWriters
             string filePath = Path.Combine(outputPath, "index.html");
 
             File.Create(filePath).Close();
-            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/helper.js"), Path.Combine(outputPath, "helper.js"), true);
-            File.Copy(Path.Combine(AppContext.BaseDirectory, "HTML DOC Templates/homePageStyles.css"), Path.Combine(outputPath, "homePageStyles.css"), true);
             StreamWriter streamWriter = new StreamWriter(filePath, false);
 
-
-            streamWriter.Write(@$"<!DOCTYPE html>
+            string output = @$"<!DOCTYPE html>
                 <html lang=""en"">
 
                 <head>
                     <meta charset=""UTF-8"">
                     <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
-                    <title>{filePath}</title>
+                    <title>{docInfo.ProjectName} Documentation</title>
                     <link rel=""stylesheet"" href=""./homePageStyles.css"">
-                    <script src=""./helper.js""></script>
+                    <link rel=""stylesheet"" href=""./sidebar.css"">
+                    <script src=""./helper.js"" defer></script>
                 </head>
-                <body>");
 
-            GenerateSideBar(streamWriter, classes, enums, interfaces, structs);
+                <body> 
+                
+                {GenerateSideBar(classes, enums, interfaces, structs,docInfo)} 
 
-            streamWriter.Write(@$"<div class=""content"">
+                <!-- Main Content -->
+                    <div class=""content"">
                         <h1>{docInfo.ProjectName}</h1>
                         <p>{docInfo.ProjectDescription}</p>
                     </div>
                 </body>
-                ");
 
-            streamWriter.Write("<html>");
+                </html>";
+
+
+            streamWriter.Write(output);
             streamWriter.Close();
             streamWriter.Dispose();
 
             return filePath;
         }
 
-        private void GenerateSideBar(StreamWriter streamWriter, ClassDeclaration[]? classes, EnumDeclaration[]? enums, InterfaceDeclaration[]? interfaces, StructDeclaration[]? structs)
+        private void GenerateClasses(string outputPath,ClassDeclaration[] classes, DocumentInformation docInfo)
         {
-            streamWriter.Write(@"
-            <div class=""sidebar"">
-                <a href=""./""><h2>My Project</h2></a>
-                    <div class=""nav-section"">
-                ");
+            foreach (ClassDeclaration current in classes)
+            {
+                string classOutput = "";
+                StreamWriter streamWriter = new StreamWriter(File.Create(Path.Combine(outputPath, $"{current.Name}.html")));
+                classOutput = GenerateClassTopBody(current);
+            }
+        }
+
+        private string GenerateClassTopBody(ClassDeclaration current)
+        {
+            //string output = @$"";
+            throw new NotImplementedException();
+        }
+
+        private string GenerateSideBar(ClassDeclaration[]? classes, EnumDeclaration[]? enums, InterfaceDeclaration[]? interfaces, StructDeclaration[]? structs, DocumentInformation docinfo)
+        {
+            string output = "";
+            output = @$"<aside class=""sidebar"">
+                            <a href=""./"">
+                                <h2>{docinfo.ProjectName}</h2>
+                            </a>
+                            <div class=""nav-section"">
+                ";
 
             if (classes != null && classes.Length > 0)
             {
-                WriteSiderbarClasses(streamWriter, classes);
+                output += WriteSiderbarClasses(classes);
             }
 
             if (interfaces != null && interfaces.Length > 0)
             {
-                WriteSiderbarInterfaces(streamWriter, interfaces);
+                output += WriteSiderbarInterfaces(interfaces);
             }
 
             if (structs != null && structs.Length > 0)
             {
-                WriteSiderbarStructs(streamWriter, structs);
+                output += WriteSiderbarStructs(structs);
             }
 
             if (enums != null && enums.Length > 0)
             {
-                WriteSiderbarEnums(streamWriter, enums);
+                output += WriteSiderbarEnums(enums);
             }
 
 
-            streamWriter.Write(@"
+            output += @"
                 </div>
-            </div>");
+            </aside>";
 
-
+            return output;
         }
 
-        private void WriteSiderbarStructs(StreamWriter writer, StructDeclaration[] structs)
+        private string WriteSiderbarStructs(StructDeclaration[] structs)
         {
-            writer.Write(@$"<button onclick=""toggleMenu('structs')"">Structs ▼</button>
-                    <div id=""structs"" class=""nav-links"">
-                    ");
+            string output = @$"<button class=""toggle-btn"" onclick=""toggleMenu('structs')"">Structs <span class=""arrow"">▼</span></button>
+            <div id=""structs"" class=""nav-links"">
+                    ";
 
 
             foreach (StructDeclaration current in structs)
             {
-                writer.Write(@$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>");
+                output += @$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>";
             }
 
 
-            writer.Write(@"       </div>
-                ");
+            output += @"       </div>
+                ";
+
+            return output;
         }
 
-        private void WriteSiderbarInterfaces(StreamWriter writer, InterfaceDeclaration[] interfaces)
+        private string WriteSiderbarInterfaces(InterfaceDeclaration[] interfaces)
         {
-            writer.Write(@$"<button onclick=""toggleMenu('interaces')"">Interfaces ▼</button>
-                    <div id=""interaces"" class=""nav-links"">
-                    ");
+
+            string output = @$"<button class=""toggle-btn"" onclick=""toggleMenu('interfaces')"">Interfaces <span class=""arrow"">▼</span></button>
+            <div id=""interfaces"" class=""nav-links"">
+                    ";
 
 
             foreach (InterfaceDeclaration current in interfaces)
             {
-                writer.Write(@$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>");
+                output += @$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>";
             }
 
 
-            writer.Write(@"       </div>
-                ");
+            output += @"       </div>
+                ";
+
+            return output;
         }
 
-        private void WriteSiderbarEnums(StreamWriter writer, EnumDeclaration[] enums)
+        private string WriteSiderbarEnums(EnumDeclaration[] enums)
         {
-            writer.Write(@$"<button onclick=""toggleMenu('enums')"">Enums ▼</button>
-                    <div id=""enums"" class=""nav-links"">
-                    ");
+            string output = @$"<button class=""toggle-btn"" onclick=""toggleMenu('enums')"">Enums <span class=""arrow"">▼</span></button>
+            <div id=""enums"" class=""nav-links"">
+                    ";
 
 
             foreach (EnumDeclaration current in enums)
             {
-                writer.Write(@$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>");
+                output += @$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>";
             }
 
 
-            writer.Write(@"       </div>
-                ");
+            output += @"       </div>
+                ";
+
+            return output;
         }
 
-        private void WriteSiderbarClasses(StreamWriter writer, ClassDeclaration[] classes)
+        private string WriteSiderbarClasses(ClassDeclaration[] classes)
         {
-            writer.Write(@$"<button onclick=""toggleMenu('classes')"">Classes ▼</button>
-                    <div id=""classes"" class=""nav-links"">
-                    ");
+            string output = @$"<button class=""toggle-btn"" onclick=""toggleMenu('classes')"">Classes <span class=""arrow"">▼</span></button>
+            <div id=""classes"" class=""nav-links"">
+                    ";
 
 
             foreach (ClassDeclaration current in classes)
             {
-                writer.Write(@$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>");
+                output += @$"<a href=""./objs/{current.Name}.html"">{current.Name}</a>";
             }
 
 
-            writer.Write(@"       </div>
-                ");
+            output += @"       </div>
+                ";
+            return output;
         }
     }
 }
