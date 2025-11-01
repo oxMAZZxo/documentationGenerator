2f728a248e06b50aa9a316b80cb629a0ab8f3cff
mario <mariosgamakapa@gmail.com>
2025-11-01 21:36:04 +0000

    Created Base Avalonia MVVM App

---
+++ DocumentationGenerator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace DocumentationGenerator.ViewModels;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
---
+++ DocumentationGenerator/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DocumentationGenerator.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
