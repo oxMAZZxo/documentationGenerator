217acaac80a83ad9311ce51faaa95c79a11cea8a
oxMAZZxo <mariosgamakapa@gmail.com>
2025-11-03 20:30:12 +0000

    Styled Main View

--- DocumentationGenerator/Views/MainWindow.axaml.cs
+++ DocumentationGenerator/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace DocumentationGenerator.Views;
 
@@ -7,5 +11,38 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void OnWindowDoubleTap(object? sender, TappedEventArgs e)
+    {
+        MaximiseWindow();
+    }
+
+    private void OnMaximiseButtonClicked(object? sender, RoutedEventArgs e)
+    {
+        MaximiseWindow();
+    }
+    
+    private void MaximiseWindow()
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            MaximiseButton.Content = "[ ]";
+        } else if (WindowState == WindowState.Normal)
+        {
+            WindowState = WindowState.Maximized;
+            MaximiseButton.Content = "[]";
+        }
+    }
+
+    private void OnMinimiseButtonClicked(object? sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void OnChromeBarPointerDown(object? sender, PointerPressedEventArgs e)
+    {
+        this.BeginMoveDrag(e);
     }
 }
