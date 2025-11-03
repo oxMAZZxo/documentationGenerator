4025eec4f90b7cf74507738544cab62742eedfdc
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2025-11-03 21:30:53 +0000

    Fixed the formatting on the XAML Extension

--- DocumentationGenerator/Views/MainWindow.axaml.cs
+++ DocumentationGenerator/Views/MainWindow.axaml.cs
@@ -18,31 +18,26 @@ public partial class MainWindow : Window
         MaximiseWindow();
     }
 
-    private void OnMaximiseButtonClicked(object? sender, RoutedEventArgs e)
-    {
-        MaximiseWindow();
-    }
+    // private void OnMaximiseButtonClicked(object? sender, RoutedEventArgs e)
+    // {
+    //     MaximiseWindow();
+    // }
     
     private void MaximiseWindow()
     {
         if (WindowState == WindowState.Maximized)
         {
             WindowState = WindowState.Normal;
-            MaximiseButton.Content = "[ ]";
         } else if (WindowState == WindowState.Normal)
         {
             WindowState = WindowState.Maximized;
-            MaximiseButton.Content = "[]";
         }
     }
 
-    private void OnMinimiseButtonClicked(object? sender, RoutedEventArgs e)
-    {
-        WindowState = WindowState.Minimized;
-    }
+    // private void OnMinimiseButtonClicked(object? sender, RoutedEventArgs e)
+    // {
+    //     WindowState = WindowState.Minimized;
+    // }
 
-    private void OnChromeBarPointerDown(object? sender, PointerPressedEventArgs e)
-    {
-        this.BeginMoveDrag(e);
-    }
+
 }
